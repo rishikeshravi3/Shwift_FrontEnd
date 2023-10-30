ed8e1e3b346ca91380803d7c8cf0eeedfa9c8c4c
rishikeshravi3 <137996382+rishikeshravi3@users.noreply.github.com>
2023-10-29 22:30:57 -0400

    create account and login activity changes

--- app/src/main/java/com/example/myapplication/CreateAccountActivity.java
+++ app/src/main/java/com/example/myapplication/CreateAccountActivity.java
@@ -2,10 +2,12 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.InputType;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class CreateAccountActivity extends AppCompatActivity {
 
@@ -23,6 +25,24 @@ public class CreateAccountActivity extends AppCompatActivity {
                 password.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
                 showPassword.setBackgroundResource(R.drawable.eye_regular);
             }
+        });
+
+        ImageView showConfirmPassword = findViewById(R.id.show_confirm_password);
+        EditText confirm_password = findViewById(R.id.confirm_password);
+        showConfirmPassword.setOnClickListener(v -> {
+            if (confirm_password.getInputType() == (InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD)) {
+                confirm_password.setInputType(InputType.TYPE_CLASS_TEXT);
+                showConfirmPassword.setBackgroundResource(R.drawable.eye_slash_regular);
+            } else {
+                confirm_password.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                showConfirmPassword.setBackgroundResource(R.drawable.eye_regular);
+            }
+        });
+
+        TextView login = findViewById(R.id.login);
+        login.setOnClickListener(v -> {
+            Intent intent = new Intent(CreateAccountActivity.this, LoginActivity.class);
+            startActivity(intent);
         });
     }
 }
