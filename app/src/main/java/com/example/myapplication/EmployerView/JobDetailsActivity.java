c53b9e3eec81c168d15f0eded69902b91e5cb14f
shaurya <shauryaguliani@gmail.com>
2023-11-24 18:25:19 -0500

    Employer_View complete

--- app/src/main/java/com/example/myapplication/EmployerView/JobDetailsActivity.java
+++ app/src/main/java/com/example/myapplication/EmployerView/JobDetailsActivity.java
@@ -3,6 +3,7 @@ package com.example.myapplication.EmployerView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.example.myapplication.R;
 
--- app/src/main/java/com/example/myapplication/EmployerView/JobDetailsActivityPreview.java
+++ app/src/main/java/com/example/myapplication/EmployerView/JobDetailsActivityPreview.java
@@ -3,6 +3,9 @@ package com.example.myapplication.EmployerView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 
 import com.example.myapplication.R;
 
@@ -11,6 +14,13 @@ public class JobDetailsActivityPreview extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_job_details_preview);
+        setContentView(R.layout.activity_applicant_view_job_description);
+
+        RelativeLayout item = (RelativeLayout)findViewById(R.id.applicant_view_job_description_detail_card_relative_layout);
+        View child=getLayoutInflater().inflate(R.layout.recent_applicants_list_item,null);
+        item.addView(child);
+
+        Button button=findViewById(R.id.applicant_view_job_description_apply_button);
+        button.setText("Post");
     }
 }
