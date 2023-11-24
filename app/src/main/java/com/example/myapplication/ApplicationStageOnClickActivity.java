db6b2bbc22af275f41bcaeed29f6e07d72fa3d87
shaurya <shauryaguliani@gmail.com>
2023-11-24 00:56:09 -0500

    Shaurya_post_iteration_2_Complete

--- app/src/main/java/com/example/myapplication/ApplicationStageOnClickActivity.java
+++ app/src/main/java/com/example/myapplication/ApplicationStageOnClickActivity.java
@@ -1,8 +1,17 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.flexbox.FlexboxLayout;
+
+import java.util.ArrayList;
+import java.util.Objects;
 
 public class ApplicationStageOnClickActivity extends AppCompatActivity {
 
@@ -10,5 +19,63 @@ public class ApplicationStageOnClickActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_application_stage_on_click);
+        String company=getIntent().getStringExtra("company");
+        String role=getIntent().getStringExtra("role");
+        String status=getIntent().getStringExtra("status");
+        String location=getIntent().getStringExtra("location");
+        String salary=getIntent().getStringExtra("salary");
+        ArrayList<String> jobType=getIntent().getStringArrayListExtra("jobType");
+        int logoID=getIntent().getIntExtra("logoId",0);
+
+        ImageView logo=findViewById(R.id.application_stage_on_click_card_image);
+        TextView companyName=findViewById(R.id.application_stage_on_click_card_company);
+        TextView roleName=findViewById(R.id.application_stage_on_click_card_role);
+        TextView statusName=findViewById(R.id.application_stage_on_click_your_status);
+        TextView locationName=findViewById(R.id.application_stage_on_click_card_location);
+        TextView salaryName=findViewById(R.id.application_stage_on_click_card_salary);
+        FlexboxLayout flexboxLayout=findViewById(R.id.application_stage_on_click_card_job_type_flexbox);
+
+        logo.setImageResource(logoID);
+        companyName.setText(company);
+        roleName.setText(role);
+        statusName.setText(status);
+        locationName.setText(location);
+        salaryName.setText(salary);
+
+        TextView[] tv=new TextView[10];
+        for(int i = 0; i< Objects.requireNonNull(jobType).size(); i++)
+        {
+            tv[i]=new TextView(getApplicationContext());
+            tv[i].setText(jobType.get(i));
+            FlexboxLayout.LayoutParams params=new FlexboxLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            params.setMargins(25,25,25,25);
+            tv[i].setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+            tv[i].setTextAppearance(R.style.key_skills);
+            tv[i].setBackground(ContextCompat.getDrawable(this,R.drawable.key_skills_border));
+            tv[i].setPadding(25,25,25,25);
+            tv[i].setLayoutParams(params);
+            flexboxLayout.addView(tv[i]);
+        }
+
+
+        assert status != null;
+        switch (status) {
+            case "Application Sent":
+                statusName.setTextAppearance(R.style.application_stage_item_status_style_sent);
+                statusName.setBackground(ContextCompat.getDrawable(this, R.drawable.application_stage_on_click_status_sent_background));
+                break;
+            case "Application Accepted":
+                statusName.setTextAppearance(R.style.application_stage_item_status_style_accepted);
+                statusName.setBackground(ContextCompat.getDrawable(this, R.drawable.application_stage_on_click_status_accepted_background));
+                break;
+            case "Application Rejected":
+                statusName.setTextAppearance(R.style.application_stage_item_status_style_rejected);
+                statusName.setBackground(ContextCompat.getDrawable(this, R.drawable.application_stage_on_click_status_rejected_background));
+                break;
+        }
+
+        ImageView backArrow=findViewById(R.id.application_stage_on_click_back_arrow);
+        backArrow.setOnClickListener(v -> finish());
+
     }
 }
--- app/src/main/java/com/example/myapplication/ApplicationStagesActivity.java
+++ app/src/main/java/com/example/myapplication/ApplicationStagesActivity.java
@@ -10,13 +10,25 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Toast;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class ApplicationStagesActivity extends AppCompatActivity {
+    //Dummy data
+    ArrayList<String> jobType=new ArrayList<>();
+    String salary="$8,000 - $20,000 / month";
+    String location="New York, United Sates";
+    //Dummy Data End
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_application_stages);
 //        test data
+        jobType.add("Remote");
+        jobType.add("Full-Time");
         ApplicationStageListData[] listData=new ApplicationStageListData[]{
                 new ApplicationStageListData("Project Manager","Amazon","Application Sent",R.drawable.email_icon),
                 new ApplicationStageListData("Software Developer", "Google", "Application Accepted",R.drawable.shwift_logo),
@@ -35,6 +47,14 @@ public class ApplicationStagesActivity extends AppCompatActivity {
             @Override
             public void onItemClick(int position, View v) {
                 Intent intent=new Intent(ApplicationStagesActivity.this, ApplicationStageOnClickActivity.class);
+                intent.putExtra("role",listData[position].getRole());
+                intent.putExtra("company",listData[position].getCompany());
+                intent.putExtra("status",listData[position].getStatus());
+                intent.putExtra("logoId",listData[position].getLogoId());
+                intent.putExtra("location",location);
+                intent.putExtra("salary",salary);
+                intent.putStringArrayListExtra("jobType",jobType);
+
                 startActivity(intent);
             }
         });
